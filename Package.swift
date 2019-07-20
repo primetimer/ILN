a90ccb3881e1d2c4183e8e30dfd0eef26b5c510d
primetimer <primetimertime@gmail.com>
2019-07-20 09:01:38 +0200

    4.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
 
 // docker run --name postgres -e POSTGRES_DB=ilndb -e POSTGRES_USER=ilnuser -e POSTGRES_PASSWORD=iln -p 5432:5432 -d postgres
 import PackageDescription
